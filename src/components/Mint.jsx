69215a62672c9fc9ea91cfdeead61c06a42eb200
Keefe Overby <you@example.com>
2022-06-14 21:30:22 -0600

    Work In Progess - WrappedUsername

--- src/components/Mint.jsx
+++ src/components/Mint.jsx
@@ -1,8 +1,11 @@
 
 
 
+
+
 // mint component
 const Mint = () => {
+    
     return (
         <div className="flex w-full justify-center items-center">
             <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -10,6 +13,19 @@ const Mint = () => {
                     <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
+                            
+            <button
+              type="button"
+        
+              className="flex flex-row justify-center items-center my-5 bg-[eth-card] p-3 rounded-full cursor-pointer hover:shadow-lg shadow-inner ">
+            
+              
+              <p className="text-white text-base font-semibold">
+                Mint
+              </p>
+            </button>
+          
+                  
                                 </div>    
                                     <div>
                                         <p className="text-white font-light text-sm">
